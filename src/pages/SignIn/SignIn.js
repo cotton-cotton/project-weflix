be8bf178726f365cda913a3cf5c8e683e0f81d13
niboo <moonevening8@gmail.com>
2022-08-07 16:39:56 +0900

    Add: 로그인 창 레이아웃

--- src/pages/SignIn/SignIn.js
+++ src/pages/SignIn/SignIn.js
@@ -1,8 +1,37 @@
 import React, { useState } from 'react';
 import * as S from './SignIn.style';
+import InputContainer from '../../components/InputContainer/InputContainer';
+import { SignInData } from '../SignIn/SignInData';
 
 const SignIn = () => {
-  return <div>signIn</div>;
+  return (
+    <S.SignInWrapper>
+      <S.SignInContainer>
+        <S.EntryContainer>
+          <S.Title>로그인</S.Title>
+          {SignInData.map(list => {
+            return (
+              <InputContainer
+                key={list.id}
+                id={list.id}
+                name={list.name}
+                placeholder={list.placeholder}
+                text={list.text}
+                type={list.type}
+              />
+            );
+          })}
+          <S.ButtonContainer>
+            <S.SignInButton>로그인</S.SignInButton>
+          </S.ButtonContainer>
+        </S.EntryContainer>
+        <S.CheckboxContainer>
+          <S.SaveInfo type="checkbox" />
+          <S.CheckboxContent>로그인 정보 저장</S.CheckboxContent>
+        </S.CheckboxContainer>
+      </S.SignInContainer>
+    </S.SignInWrapper>
+  );
 };
 
 export default SignIn;
